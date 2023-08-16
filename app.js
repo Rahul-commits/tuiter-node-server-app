48713eb6127793a070048a770a4a1a6bcc48efe3
Rahul-commits <rahul.narramaneni@gmail.com>
2023-08-15 22:47:08 -0400

    a6 Mongo su23

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import express from 'express';
 import cors from 'cors'
 import session from "express-session";
 import AuthController from "./users/auth-controller.js";
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
 
 import UserController from "./users/users-controller.js";
 import HelloController from "./controllers/hello-controller.js";
